986ed3e27078cda9433841bc5e6213fe573ba954
Michal Szajter <michal@szajter.cz>
2018-08-06 11:49:49 +0200

    test(): implement Cypress.io dashboard service

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,8 +16,4 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
-Cypress.Screenshot.defaults({
-  screenshotOnRunFailure: false
-});
-
 Cypress.config('mode', `${Cypress.env('mode') || 'esm'}`);
