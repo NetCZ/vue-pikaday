28359548cccd5d86e56a4e1fdf60f196b2d90a50
Michal Szajter <michal@szajter.cz>
2020-04-29 18:33:14 +0200

    fix: fix directive event trigger

--- src/directives.js
+++ src/directives.js
@@ -12,11 +12,11 @@ export const VuePikadayVisible = {
   inserted(el: Object, binding: Object, VNode: Object) {
     const instance: Object = VNode.componentInstance;
 
-    instance.onOpen = () => changeState(true, VNode.context, binding.expression, instance);
+    instance.onOpen = () => _debounce(() => changeState(true, VNode.context, binding.expression, instance), 100);
 
     instance.onClose = _debounce(() => changeState(false, VNode.context, binding.expression, instance), 100);
   },
   update(el: Object, binding: Object, VNode: Object) {
-    changeState(binding.value, VNode.componentInstance, binding.expression);
+    _debounce(() => changeState(binding.value, VNode.componentInstance, binding.expression), 100)();
   }
 };
