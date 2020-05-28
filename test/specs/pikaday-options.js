456f2ccbcd770315e3cccab61bd7a842a81edef7
Michal Szajter <michal@szajter.cz>
2020-05-28 21:23:02 +0200

    test: add reactive options tests

--- test/specs/pikaday-options.js
+++ test/specs/pikaday-options.js
@@ -1,15 +1,18 @@
 import moment from 'moment';
 
 const allowedDateStart = moment().date(5);
-const allowedDateEnd = moment(allowedDateStart).add(7, 'days');
+const allowedDateEnd = moment(allowedDateStart).add(6, 'days');
 
 const component = {
-  template: `<vue-pikaday 
+  template: `<div>
+    <vue-pikaday 
       v-model="date" 
       :auto-default="true"
-      :options="options" 
-      data-vue-pikaday 
-    ></vue-pikaday>`,
+      :options="options"
+      data-vue-pikaday
+    ></vue-pikaday>
+    <button @click="updateOptions()" data-test="update-options">Update options</button>
+  </div>`,
   data() {
     return {
       options: {
@@ -18,6 +21,12 @@ const component = {
         maxDate: allowedDateEnd.toDate()
       }
     };
+  },
+  methods: {
+    updateOptions() {
+      this.options.minDate = moment(allowedDateStart).subtract(2, 'day').toDate();
+      this.options.maxDate = moment(allowedDateEnd).subtract(2, 'day').toDate();
+    }
   }
 };
 
@@ -44,7 +53,7 @@ describe('Pikaday options', () => {
     cy.get('@picker').should('have.value', allowedDateStart.format('YYYY/MM/DD'));
   });
 
-  it('restricts dates selection to current week', () => {
+  it('restricts dates selection to specific week', () => {
     cy.get('[data-vue-pikaday]').as('picker').click();
     cy.get(`[data-day="${ moment(allowedDateStart).subtract(1, 'day').date() }"]`).should('have.class', 'is-disabled');
 
@@ -57,5 +66,22 @@ describe('Pikaday options', () => {
     cy.get(`[data-day="${ plus5Days.date() }"]`).should('not.have.class', 'is-disabled');
     cy.get(`[data-day="${ plus5Days.date() }"] button`).click();
     cy.get('@picker').should('have.value', plus5Days.format('YYYY/MM/DD'));
+  });
+
+  it('updates dates restriction selection', () => {
+    cy.get('[data-test="update-options"]').click();
+
+    cy.get('[data-vue-pikaday]').as('picker').click();
+    cy.get(`[data-day="${ moment(allowedDateStart).subtract(3, 'days').date() }"]`).should('have.class', 'is-disabled');
+
+    cy.get('@picker').click();
+    cy.get(`[data-day="${ moment(allowedDateEnd).subtract(1, 'day').date() }"]`).should('have.class', 'is-disabled');
+
+    const plus2Days = moment(allowedDateStart).add(2, 'days');
+
+    cy.get('@picker').click();
+    cy.get(`[data-day="${ plus2Days.date() }"]`).should('not.have.class', 'is-disabled');
+    cy.get(`[data-day="${ plus2Days.date() }"] button`).click();
+    cy.get('@picker').should('have.value', plus2Days.format('YYYY/MM/DD'));
   });
 });
